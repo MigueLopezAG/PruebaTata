f094d5b5869aad4d202435936a3ece67cd29a2e7
Miguel Lopez <miguel.lpz.g95@gmail.com>
2021-11-23 17:08:46 -0600

    routes changes

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 import { get, getAll, postUser, updateUser} from '../../controllers/userController';
 
-router.get('/', getAll);
-router.get('/:id', get);
-router.post('/', postUser);
-router.patch('/:id', updateUser)
+router.get('/view', getAll);
+router.get('/view/:id', get);
+router.post('/add', postUser);
+router.patch('/edit/:id', updateUser)
 
 export default router;
